cd66d6733bd2560f7587854739d747afb6ace848
minseb96 <minseb96@naver.com>
2022-06-09 02:10:17 +0900

    ~To Do List part Two / Local backup of my macbook

--- react-for-beginners2/src/App.js
+++ react-for-beginners2/src/App.js
@@ -1,12 +1,40 @@
-import Button from "./Button";
+import { useState } from "react";
 
-function App() {
+const App = () => {
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => {
+    setToDo(event.target.value);
+  };
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === "") {
+      return;
+    }
+    setToDo("");
+    setToDos((currentArray) => [toDo, ...currentArray]);
+  };
+  console.log(toDos);
   return (
     <div>
-      <h1>Welcome back!</h1>
-      <Button text={"Continue"}/>
+      <h1>My To Dos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDo}
+          type="text"
+          placeholder="Write your to do..."
+        />
+        <button>Add To Do</button>
+      </form>
+      <hr />
+      <ul>
+        {toDos.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
     </div>
   );
-}
+};
 
 export default App;
